b0f5e5ee28fd2e364ab7a276dfd752f2bb3d8acf
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-04 07:33:26 +0600

    dynamic router setup and Sidber dynamic content show

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
+import Landing from "@/components/Landing";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="">Landing page</div>
+   <div className=""><Landing /> </div>
   );
 }
