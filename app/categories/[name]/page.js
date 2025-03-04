f46ff89202fb08eefa749f69f7ad19bf6d02feb0
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-04 07:50:05 +0600

    authors, categories,tags er dynamic route setup

---
+++ app/categories/[name]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategoriesPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default CategoriesPage
---
+++ app/tags/[name]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TagsPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default TagsPage
