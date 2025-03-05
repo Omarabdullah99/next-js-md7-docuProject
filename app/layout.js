64fc29e846ca0647f055d35111a01af1879869ab
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-03-05 16:28:03 +0600

    loading handle logic done

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { getDocument } from "@/lib/doc";
 import Header from "@/components/Header";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +31,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="h-full lg:ml-72 xl:ml-80">
+        <Suspense fallback={<Loading />}>
           <Header docs={allDocuement}/>
           <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
             <main className="flex-auto py-12">
@@ -41,6 +45,7 @@ export default function RootLayout({ children }) {
               {children}
             </main>
           </div>
+          </Suspense>
         </div>
       </body>
     </html>
